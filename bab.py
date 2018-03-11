568b9aec8e2d802d5f7a33edfb10c396a48f26d2
Steve Theodore <steve@theodox.com>
2018-03-11 01:40:59 -0800

    fixed supremely dumb bug in module constructor shim; this version renders

--- bab.py
+++ bab.py
@@ -1,44 +1,43 @@
 import org.babylonjs as babylon
 from org.transcrypt.stubs.browser import document
 
-
-
-
 PIOVERTWO = Math.PI / 2.0
 
-canvas = document.getElementById("renderCanvas")
+def main():
+    canvas = document.getElementById("renderCanvas")
+    engine = babylon.Engine(canvas, True)
 
-engine = babylon.Engine(canvas, True)
+    def setup():
+        scene = babylon.Scene(engine)
+        test = babylon.Vector3.Zero()
+        console.log(test)
 
+        #Add a camera to the scene and attach it to the canvas
+        camera = babylon.ArcRotateCamera("Camera", PIOVERTWO, PIOVERTWO, 2, babylon.Vector3(0,0,0), scene)
+        camera.attachControl(canvas, True)
 
-scene = babylon.Scene(engine)
+        # Add lights to the scene
+        light1 = babylon.HemisphericLight("light1", babylon.Vector3(1, 1, 0), scene)
+        light2 = babylon.PointLight("light2", babylon.Vector3(0, 1, -1), scene)
 
-#Add a camera to the scene and attach it to the canvas
-camera = babylon.ArcRotateCamera("Camera", PIOVERTWO, PIOVERTWO, 2, babylon.Vector3(0,0,0), scene)
-camera.attachControl(canvas, True)
+        #This is where you create and manipulate meshes
 
-# Add lights to the scene
-light1 = babylon.HemisphericLight("light1", babylon.Vector3(1, 1, 0), scene)
-light2 = babylon.PointLight("light2", babylon.Vector3(0, 1, -1), scene)
+        sphere = babylon.MeshBuilder.CreateSphere("sphere", {}, scene)
+        opts = {
+            'size': 4,
+            'width': 4,
+            'height': 4,
+            'sourcePlane': babylon.Plane(0,-1,0,1)
+            }
+        box = babylon.MeshBuilder.CreatePlane("plane", opts, scene )
+        return scene
 
-#This is where you create and manipulate meshes
+    scene_object = setup()
 
-sphere = babylon.MeshBuilder.CreateSphere("sphere", {}, scene)
-opts = {
-    'size': 4,
-    'width': 4,
-    'height': 4,
-    'sourcePlane': babylon.Plane(0,-1,0,1)
-}
-box = babylon.MeshBuilder.CreatePlane("plane", opts, scene )
+    def callback():
+        scene_object.render()
+    engine.runRenderLoop(callback)
 
-def cb():
-    sphere.translate(v, .01, babylon.BABYLON.Space.Local)
-    v.x += .001
-    v.normalize()
+    window.addEventListener("resize", engine.resize)
 
-    return scene.render()
-
-engine.runRenderLoop(cb)
-
-window.addEventListener("resize", engine.resize)
+main()
--- org/babylonjs/__init__.py
+++ org/babylonjs/__init__.py
@@ -1,30 +1,18 @@
 __pragma__ ('noanno')
 def _cls(obj, *statics):
 
-	def _c_( *args):
-		return __new__(obj, (*args))
-
+	def _c_( *args ):
+		return __new__(obj(*args))
 	for s in statics:
 		_c_[s] = obj[s]
-	
 	return _c_
 
-# class BabylonBase(type):
-
-# 	def __new__(meta, bases, namespace):
-# 		parent_class = namespace.pop('base')
-# 		def _c_(self, *args):
-# 			self.__instance__ = __new__(parent_class, *args)
-# 		namespace['__init__'] = _c_
-# 	return type.__new__(meta, bases, namespace)
-
-#https://cdn.babylonjs.com/babylon.js
-
 __pragma__ ('js',
-    '{}',
-    __include__('org/babylonjs/__javascript__/babylon.custom.js')
-)
-
+	'''(function () {{
+    var exports = {{}};
+    {} 
+    return exports;
+}}) () .fabric;''', __include__('org/babylonjs/__javascript__/babylon.custom.js'))
 
 Engine = _cls(BABYLON.Engine)
 Scene = _cls(BABYLON.Scene)
