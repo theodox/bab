7294981b70992ebf1ecd0cfcb18c3867e6b892bb
Steve Theodore <steve@theodox.com>
2018-03-11 23:00:20 -0700

    working again, after experimenting with operator overloads

--- bab.py
+++ bab.py
@@ -3,41 +3,46 @@ from org.transcrypt.stubs.browser import document
 
 PIOVERTWO = Math.PI / 2.0
 
+
 def main():
     canvas = document.getElementById("renderCanvas")
     engine = babylon.Engine(canvas, True)
 
     def setup():
         scene = babylon.Scene(engine)
-        test = babylon.Vector3.Zero()
-        console.log(test)
 
-        #Add a camera to the scene and attach it to the canvas
-        camera = babylon.ArcRotateCamera("Camera", PIOVERTWO, PIOVERTWO, 2, babylon.Vector3(0,0,0), scene)
+        # Add a camera to the scene and attach it to the canvas
+        camera = babylon.ArcRotateCamera("Camera", PIOVERTWO, PIOVERTWO, 2, babylon.Vector3(0, 0, 0), scene)
         camera.attachControl(canvas, True)
 
         # Add lights to the scene
         light1 = babylon.HemisphericLight("light1", babylon.Vector3(1, 1, 0), scene)
         light2 = babylon.PointLight("light2", babylon.Vector3(0, 1, -1), scene)
 
-        #This is where you create and manipulate meshes
+        # This is where you create and manipulate meshes
 
         sphere = babylon.MeshBuilder.CreateSphere("sphere", {}, scene)
         opts = {
             'size': 4,
             'width': 4,
             'height': 4,
-            'sourcePlane': babylon.Plane(0,-1,0,1)
-            }
-        box = babylon.MeshBuilder.CreatePlane("plane", opts, scene )
-        return scene
+            'sourcePlane': babylon.Plane(0, -1, 0, 1)
+        }
+        box = babylon.MeshBuilder.CreatePlane("plane", opts, scene)
+        return scene, sphere
 
-    scene_object = setup()
+    scene_object, sphere = setup()
 
+    dummy = 0
+    delta = babylon.Vector3.Up().scaleInPlace(0.001)
     def callback():
+        dummy += 1
+        sphere.position  = sphere.position + delta #__:fopov
         scene_object.render()
+
     engine.runRenderLoop(callback)
 
     window.addEventListener("resize", engine.resize)
+
 
 main()
--- org/babylonjs/__init__.py
+++ org/babylonjs/__init__.py
@@ -1,18 +1,23 @@
-__pragma__ ('noanno')
-def _cls(obj, *statics):
+__pragma__('noanno')
 
-	def _c_( *args ):
-		return __new__(obj(*args))
-	for s in statics:
-		_c_[s] = obj[s]
-	return _c_
 
-__pragma__ ('js',
-	'''(function () {{
-    var exports = {{}};
-    {} 
-    return exports;
-}}) () .fabric;''', __include__('org/babylonjs/__javascript__/babylon.custom.js'))
+def _cls(obj):
+    def _c_(*args):
+        return __new__(obj(*args))
+
+    # allows for 'static' functions too
+    Object.setPrototypeOf(_c_, obj)
+
+    obj['__str__'] = object.toString
+    return _c_
+
+
+__pragma__('js',
+           '''(function () {{
+           var exports = {{}};
+           {} 
+           return exports;
+       }}) () .fabric;''', __include__('org/babylonjs/__javascript__/babylon.custom.js'))
 
 Engine = _cls(BABYLON.Engine)
 Scene = _cls(BABYLON.Scene)
@@ -20,6 +25,15 @@ Camera = _cls(BABYLON.Camera)
 ArcRotateCamera = _cls(BABYLON.ArcRotateCamera)
 HemisphericLight = _cls(BABYLON.HemisphericLight)
 PointLight = _cls(BABYLON.PointLight)
-Vector3 = _cls(BABYLON.Vector3, 'Zero')
-MeshBuilder = BABYLON.MeshBuilder 
+MeshBuilder = BABYLON.MeshBuilder
 Plane = _cls(BABYLON.Plane)
+Vector3 = _cls(BABYLON.Vector3)
+
+BABYLON.Vector3.prototype['__add__'] = BABYLON.Vector3.prototype.add
+BABYLON.Vector3.prototype['__iadd__'] = BABYLON.Vector3.prototype.addInPlace
+BABYLON.Vector3.prototype['__sub__'] = BABYLON.Vector3.prototype.subtract
+BABYLON.Vector3.prototype['__isub__'] = BABYLON.Vector3.prototype.subtractInPlace
+BABYLON.Vector3.prototype['__mul__'] = BABYLON.Vector3.prototype.multiply
+BABYLON.Vector3.prototype['__imul__'] = BABYLON.Vector3.prototype.multiplyInPlace
+BABYLON.Vector3.prototype['__div__'] = BABYLON.Vector3.prototype.divide
+BABYLON.Vector3.prototype['__idiv__'] = BABYLON.Vector3.prototype.divideInPlace
