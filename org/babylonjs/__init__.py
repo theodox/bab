ca9a7828b21305dc5c0c1e5f5a11a97b54580780
Steve Theodore <steve@theodox.com>
2018-03-17 00:47:59 -0700

    added overload support

--- org/babylonjs/__init__.py
+++ org/babylonjs/__init__.py
@@ -1,7 +1,10 @@
 __pragma__('noanno')
 
 
-def _cls(obj):
+def _js_class(obj):
+    '''
+    Add python-style constructor, but preserve static methods from the original class
+    '''
     def _c_(*args):
         return __new__(obj(*args))
 
@@ -11,20 +14,37 @@ def _cls(obj):
     obj['__str__'] = object.toString
     return _c_
 
+__pragma__('kwargs')
+
+def _js_math_class(obj, add ='add', subtract ='subtract', multiply ='multiply', divide ='divide'):
+    '''
+    Add python constructor and python magic methods for operator overloading
+    '''
+    obj.prototype['__add__'] = obj.prototype[add]
+    obj.prototype['__sub__'] = obj.prototype[subtract]
+    obj.prototype['__mul__'] = obj.prototype[multiply]
+    obj.prototype['__div__'] = obj.prototype[divide]
+
+    base = _js_class(obj)
+    return base
+
+__pragma__('nokwargs')
 
 __pragma__('js', '{}' , __include__('org/babylonjs/__javascript__/babylon.custom.js'))
 
-Engine = _cls(BABYLON.Engine)
-Scene = _cls(BABYLON.Scene)
-Camera = _cls(BABYLON.Camera)
-ArcRotateCamera = _cls(BABYLON.ArcRotateCamera)
-HemisphericLight = _cls(BABYLON.HemisphericLight)
-PointLight = _cls(BABYLON.PointLight)
-MeshBuilder = BABYLON.MeshBuilder
-Plane = _cls(BABYLON.Plane)
-Vector3 = _cls(BABYLON.Vector3)
+Engine = _js_class(BABYLON.Engine)
+Scene = _js_class(BABYLON.Scene)
+Camera = _js_class(BABYLON.Camera)
 
-BABYLON.Vector3.prototype['__add__'] = BABYLON.Vector3.prototype.add
-BABYLON.Vector3.prototype['__sub__'] = BABYLON.Vector3.prototype.subtract
-BABYLON.Vector3.prototype['__mul__'] = BABYLON.Vector3.prototype.multiply
-BABYLON.Vector3.prototype['__div__'] = BABYLON.Vector3.prototype.divide
+class cameras:
+    ArcRotateCamera = _js_class(BABYLON.ArcRotateCamera)
+
+class lights:
+    HemisphericLight = _js_class(BABYLON.HemisphericLight)
+    PointLight = _js_class(BABYLON.PointLight)
+MeshBuilder = BABYLON.MeshBuilder
+Plane = _js_class(BABYLON.Plane)
+
+# math classes use _opov_cls
+Vector3 = _js_math_class(BABYLON.Vector3)
+
