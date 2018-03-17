bc3510ec03489db3ceb75bd5ba9c688e90241016
= <=>
2018-03-17 11:25:01 -0700

    begin subdividing module

--- org/babylonjs/cameras.py
+++ org/babylonjs/cameras.py
@@ -2,3 +2,16 @@ from org.babylonjs.utils import _js_class, _js_math_class, BABYLON
 
 Camera = _js_class(BABYLON.Camera)
 ArcRotateCamera = _js_class(BABYLON.ArcRotateCamera)
+DeviceOrientationCamera = _js_class(BABYLON.DeviceOrientationCamera)
+FollowCamera = _js_class(BABYLON.FollowCamera)
+FreeCamera=_js_class(BABYLON.FreeCamera)
+GamepadCamera = _js_class(BABYLON.GamepadCamera)
+TargetCamera = _js_class(BABYLON.TargetCamera)
+TouchCamera - _js_class(BABYLON.TouchCamera)
+VirtualJoysticksCamera = _js_class(BABYLON.VirtualJoysticksCamera)
+
+AnaglyphArcRotateCamera = _js_class(BABYLON.ArcRotateCamera)
+AnaglyphFreeCamera=_js_class(BABYLON.AnaglyphFreeCamera)
+AnaglyphFreeCamera=_js_class(BABYLON.AnaglyphFreeCamera)
+
+VRDeviceOrientationCamera = _js_class(BABYLON.VRDeviceOrientationCamera)
--- org/babylonjs/lights.py
+++ org/babylonjs/lights.py
@@ -1,4 +1,7 @@
 from org.babylonjs.utils import _js_class, _js_math_class, BABYLON
 
+Light=_js_class(BABYLON.Light)
 HemisphericLight = _js_class(BABYLON.HemisphericLight)
 PointLight = _js_class(BABYLON.PointLight)
+DirectionalLight = _js_class(BABYLON.DirectionalLight)
+SpotLight=_js_class(BABYLON.SpotLight)
