168ceb7305116cbf455b770f7c33174aec4ca463
= <=>
2018-06-01 23:16:22 -0700

    new generalized property factory

---
+++ org/babylonjs/gui.py
@@ -0,0 +1,21 @@
+from org.babylonjs.reflection import ClassFactory, construct
+import logging
+logger = logging.getLogger(__name__)
+
+
+def _gui_class_factory(api_key, api_object):
+    '''
+    Add python-style constructor, but preserve static methods from the original class
+    '''
+    __pragma__('ifndef', 'release')
+    assert (api_object is not None)
+    logger.debug(api_key)
+    __pragma__('endif')
+
+    BabylonGUIObject = construct(api_object, api_key, __name__)
+
+
+    return api_key, BabylonGUIObject
+
+
+ClassFactory(window.BABYLON.GUI, __all__).reflect(_gui_class_factory)
---
+++ org/babylonjs/reflection.py
@@ -0,0 +1,35 @@
+
+def construct(cls, cls_name, module):
+    """returns a wrapping constructor coll for 'cls', which should be am api object"""
+    def __init__(*args):
+        return __new__(cls(*args))
+
+    __init__.__name__ = cls_name
+    __init__.__str__ = lambda self: "<class '{}.{}'>".format(module, cls_name)
+
+    cls.prototype.__str__ = lambda s: "<{}>".format(cls_name)
+
+    return __init__
+
+
+class ClassFactory:
+
+    def __init__(self, original, namespace):
+        self.namespace = namespace
+        self.original = original
+
+    def reflect(self, handler, namespace):
+
+        __pragma__('jsiter')
+
+        for eachobj in self.original:
+            if not eachobj.startswith('_'):
+                proto = eachobj.prototype if eachobj.hasOwnProperty('prototype') else None
+                api_obj = self.original[eachobj]
+                new_name, result = handler(eachobj, api_obj)
+                if result is not None:
+                    self.namespace[new_name] = result
+                    api_obj.prototype.__class__ = result
+                    window.Object.setPrototypeOf(result, api_obj)
+
+        __pragma__('nojsiter')
